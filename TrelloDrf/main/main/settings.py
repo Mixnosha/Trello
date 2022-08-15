2ae35d36fb40564de4695231c52df7fade46ee67
mixnosha <xmixho@gmail.com>
2022-08-15 22:46:24 +0300

    create all App

--- TrelloDrf/main/main/settings.py
+++ TrelloDrf/main/main/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'boardsApp',
+    'cardsApp',
+    'columnApp',
+    'commentApp',
+    'userApp',
+    'workSpacesApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trello_db',
+        'USER': 'trello_user',
+        'PASSWORD': 'Maksim21z',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
