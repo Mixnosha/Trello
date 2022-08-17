62ebe523bd4f24e9f0144f2002cd05e4eb5e6ed8
mixnosha <xmixho@gmail.com>
2022-08-17 23:59:51 +0300

    register_add

--- TrelloDrf/main/authUser/serializers.py
+++ TrelloDrf/main/authUser/serializers.py
@@ -3,5 +3,6 @@ from rest_framework import serializers
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField()
     email = serializers.EmailField()
-    password = serializers.CharField()
-    first_name = serializers.CharField()
+    password1 = serializers.CharField()
+    password2 = serializers.CharField()
+    # first_name = serializers.CharField()
--- TrelloDrf/main/authUser/service.py
+++ TrelloDrf/main/authUser/service.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-
+from authUser.forms import UserRegisterForm
 from userApp.models import CustomUser
 
 
 def registerUser(data):
     try:
-        User.objects.create(username=data['username'], email=data['email'], password=data['password'])
-        # CustomUser.objects.create()
+        form = UserRegisterForm(data)
+        form.save()
+        o
+        user = User.objects.get(username=data['username'])
+        CustomUser.objects.create(user=user.username, first_name=data['first_name'])
         return True
     except IntegrityError:
         return 'A user with the same name already exists'
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
 
 
