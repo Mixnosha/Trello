b7d689320f9640196b670a65a5cd37ef37dd7867
mixnosha <xmixho@gmail.com>
2022-08-19 21:47:57 +0300

    auto doc with drf-yasg

--- TrelloDrf/main/main/urls.py
+++ TrelloDrf/main/main/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from .jasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +8,5 @@ urlpatterns = [
     path('api/v1/', include('workSpacesApp.urls')),
 
 ]
+
+urlpatterns += doc_urls
--- TrelloDrf/main/workSpacesApp/views.py
+++ TrelloDrf/main/workSpacesApp/views.py
@@ -1,7 +1,18 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, permissions
 from workSpacesApp.models import WorkSpaces
 from workSpacesApp.serializers import WorkSpaceCreateSerializer, ViewOneWorkspacesSerializer
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Получение всех рабочих пространств залогиненого пользователя"
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_description="Создание рабочего пространства"
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Получение одного рабочего пространства залогиненного пользователя"
+))
 class WorkspaceModelView(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
